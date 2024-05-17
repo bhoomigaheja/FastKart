a8d2599946405d8ad9b91792b005b7488411a184
bhoomi gaheja <bhoomigaheja.ps12@gmail.com>
2024-05-17 18:06:40 +0530

    first commit

--- ecom/models.py
+++ ecom/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     product_image= models.ImageField(upload_to='product_image/',null=True,blank=True)
     price = models.PositiveIntegerField()
     description=models.CharField(max_length=40)
-    quantity = models.PositiveIntegerField(default=0)
+    
     def __str__(self):
         return self.name
 
